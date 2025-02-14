fa3de9d881f53d5580547a8046e1b6998ab45004
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2025-02-14 16:08:07 -0300

    feat: add object of selectors to keep test more constant and semantic

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,21 +1,29 @@
 describe("Orange HRM Tests", () => {
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[type='password']",
+    buttonSubmit: "[type='submit']",
+    errorAlert: ".oxd-alert--error",
+    dashboardTitle: ".oxd-topbar-header-breadcrumb > .oxd-text",
+  };
+
   it("Login - success", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get("[name='username']").type("Admin");
-    cy.get("[type='password']").type("admin123");
-    cy.get("[type='submit']").click();
+    cy.get(selectorsList.usernameField).type("Admin");
+    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.buttonSubmit).click();
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
-    cy.get(".oxd-topbar-header-breadcrumb-module").contains("Dashboard");
+    cy.get(selectorsList.dashboardTitle).contains("Dashboard");
   });
   it("Login - fail", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get("[name='username']").type("Test");
-    cy.get("[type='password']").type("Test");
-    cy.get("[type='submit']").click();
-    cy.get(".oxd-alert--error");
+    cy.get(selectorsList.usernameField).type("Test");
+    cy.get(selectorsList.passwordField).type("Test");
+    cy.get(selectorsList.buttonSubmit).click();
+    cy.get(selectorsList.errorAlert);
   });
 });
