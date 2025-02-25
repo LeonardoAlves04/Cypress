41cea1b754ef152cb277db4369c1cbdaf323cbad
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2025-02-25 20:11:01 -0300

    feat: add chance packet to ramdon values on user spec

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import LoginPage from "../pages/loginPage";
 import MenuPage from "../pages/menuPage";
 import MyInfoPage from "../pages/myInfoPage";
 
+const Chance = require("chance");
+
+const chance = new Chance();
 const loginPage = new LoginPage();
 const dashboardPage = new DashboardPage();
 const menuPage = new MenuPage();
@@ -18,7 +21,7 @@ describe("Orange HRM Tests", () => {
     );
     dashboardPage.checkDashboardPage();
     menuPage.acessMyInfo();
-    myInfoPage.NameCamp("First Name", "Last Name", "Nickname");
+    myInfoPage.NameCamp(chance.first(), chance.last(), chance, string());
     myInfoPage.Idcamp("EmployeeId", "OtherId", "2004-02-21", "2004-02-21");
     myInfoPage.AdditionalInformation("2004-02-21");
     myInfoPage.saveForm();
